6bf6968179897e0fad243965cdf764d5098cba49
codexakshay <codexakshay@gmail.com>
2020-09-15 12:58:05 +0530

    UC4_GiTHuB_19

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,27 +1,22 @@
-public class EmployeeWage
-{
+public class EmployeeWage {
     public static final int WAGE_PER_HR=20,PART_TIME=1,FULL_DAY=2;
     public static void main(String[] args) {
-
         int random=(int)Math.floor(Math.random()*10)%3;
         int workingHr=0,salary=0;
-
-        if(random == FULL_DAY)
-                {
-                        System.out.println("EMPLOYEE PRESENT FOR FULL DAY");
-                        workingHr=8;
-                }
-
-         if(random == PART_TIME)
-                {
-                        System.out.println("EMPLOYEE PRESENT FOR HALF DAY");
-                        workingHr=4;
-         else
-                        System.out.println("EMPLOYEE IS ABSENT");
-
-                }
-
-                salary=WAGE_PER_HR*workingHr;
-                System.out.println("EMPLOYEE WAGE : "+salary);
+        switch (random) {
+            case FULL_DAY :
+                System.out.println("EMPLOYEE PRESENT FOR FULL DAY");
+                workingHr=8;
+                break;
+            case PART_TIME :
+                System.out.println("EMPLOYEE PRESENT FOR HALF DAY");
+                workingHr=4;
+                break;
+            default :
+                System.out.println("EMPLOYEE IS ABSENT");
+                break;
+        }
+        salary=WAGE_PER_HR*workingHr;
+        System.out.println("EMPLOYEE WAGE : "+salary);
     }
 }
