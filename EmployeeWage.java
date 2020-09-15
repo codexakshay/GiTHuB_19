27769761e6f4230934f7b29662d1faa8c84cb502
codexakshay <codexakshay@gmail.com>
2020-09-15 12:55:02 +0530

    UC2_GiTHuB_19

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,19 @@
 public class EmployeeWage
 {
-        public static final int  PRESNT=2;
+        public static final int WAGE_PER_HR=20,HALF_DAY=1,FULL_DAY=2;
         public static void main(String[] args)
         {
-        int random=(int)Math.floor(Math.random()*10)%3;
-
-                if(random == PRESENT)
+                int random=(int)Math.floor(Math.random()*10)%3;
+                int workingHr=0,salary=0;
+                if(random == FULL_DAY)
                 {
-                        System.out.println("Employee is PRESENT");
+                        System.out.println("Employee is PRESENT for Full Day");
+                        workingHr=8;
                 else
-                        System.out.println("Employee is ABSENT");
+                        System.out.println("employee is absent");
                 }
+
+                salary=WAGE_PER_HR*workingHr;
+                System.out.println("Employee Wage: "+salary);
         }
 }
